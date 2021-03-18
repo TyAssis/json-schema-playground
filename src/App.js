d1bc22b2556e8f0ffca79d3b20352acb6ae62eeb
taylon <tyassis3@gmail.com>
2021-03-18 10:58:34 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "@rjsf/core";
+import Ajv from 'ajv';
 
 function App() {
+  const schema = {
+    "cidade": {
+      "type": "string",
+      "enum": [],
+    },
+    "definitions": {
+      "$ref": "#definitions/relacao-pais-cidade-estado",
+    },
+    "$id": "http://gupy.io/s3-schema-pais-cidade-estado",
+  };
+  const ajv = new Ajv();
+  const a = ajv.getSchema("http://json-schema.org/draft-04/schema") || ajv.compile();
+  console.log(a);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<Form schema={schema}*/}
+      {/*    onChange={console.log("changed")}*/}
+      {/*    onSubmit={console.log("submitted")}*/}
+      {/*    onError={console.log("errors")}*/}
+      {/*/>*/}
     </div>
   );
 }
